27895d12dddbca01448f0f8cf7be49e2bc5e9fb4
Marcel Ramos <marceldavid@hotmail.com>
2026-02-25 16:12:17 -0500

    Create hero section and insert video

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </div>
   );
 }
