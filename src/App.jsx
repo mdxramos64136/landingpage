0ab213c367f044a2cf0eb1db899f827e00dbe05c
Marcel Ramos <marceldavid@hotmail.com>
2026-02-25 18:36:25 -0500

    Create workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import Features from "./components/Features";
 import Navbar from "./components/Navbar";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <Features />
+        <Workflow />
       </div>
     </div>
   );
